88b138a5b1de471097f0ff6963a675f40dc645ea
RokiSd <wrx76067@student.wsb.wroclaw.pl>
2023-05-12 18:58:42 +0200

    task1

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+
+  provisioner "local-exec" {
+    command = "python welcome.py"
+  }
+}
